43d8acfbc9b8b7bce8024f9d42f9a50a31152e12
warrentd <twarren@phoenixhawaii.net>
2021-08-13 23:26:20 -0400

    Built Database Structure

--- server/database.sql
+++ server/database.sql
@@ -3,11 +3,20 @@ CREATE DATABASE pernjwttodo;
 --set extension
 --create extension if not exists "uuid-ossp";
 
+--user table
 CREATE TABLE users(
     user_id uuid primary key default uuid_generate_v4(),
     user_name varchar(255) not null,
-    user_email varchar(255) not null,
+    user_email varchar(255) not null unique,
     user_password varchar(255) not null
+);
+
+--todo table
+CREATE TABLE todos(
+    todo_id serial primary key,
+    user_id uuid,
+    description varchar(255) not null,
+    foreign key (user_id) references users(user_id)
 );
 
 --insert fake users
