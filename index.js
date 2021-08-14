c1c2d39ffd7ef7b8a03d2b154b6eb3d001e4f38e
warrentd <twarren@phoenixhawaii.net>
2021-08-14 03:59:23 -0400

    Replaced .env file

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+//process.env.PORT
+//process.env.NODE_ENV => production or undefined
 
 //middleware
 app.use(express.json())
 app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+    //serve static content
+    //npm run build
+    app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
 //routes
 //register and login routes
@@ -13,6 +25,6 @@ app.use('/auth', require('./routes/jwtAuth'));
 //dashboard route
 app.use('/dashboard', require('./routes/dashboard'));
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
